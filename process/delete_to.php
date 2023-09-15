e0f9b358130fcdbf78282d7f36fb14b23caab777
ebusy9 <g404ebus@gmail.com>
2023-09-15 10:22:57 +0200

    des modif

---
+++ process/delete_to.php
@@ -0,0 +1,25 @@
+<?php
+
+$data = json_decode($json);
+
+use class\Manager;
+
+include_once __DIR__ . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "autoload.php";
+include_once __DIR__ . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "db.php"; 
+
+$manager = new Manager($db);
+
+if (isset($_GET['id'])) {
+    $review = (new Manager($db))->deleteDestinationByOperatorId($_GET['id']);
+    $currentFile = __FILE__;
+    if ($review) {
+        header("Location:../location.php?name={$_POST['locationName']}&info=delteTOSuccess");
+        die();
+    } else {
+        header('Location:../location.php?redirectedFrom={$currentFile}&info=delteTOFailed');
+        die();
+    }
+} else {
+    header('Location:../location.php?redirectedFrom={$currentFile}&info=idIsNotSet');
+    die();
+}
